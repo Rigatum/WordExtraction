e57f237afae8bb976d16c4f1e7ae4d7a7334c126
Tagir Melnikov <Melnikov@gmail.ru>
2023-08-12 08:41:01 +0700

    add extract page

---
+++ WordExtraction/Controllers/Extract.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WordExtraction.Controllers;
+
+public class Extract : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}

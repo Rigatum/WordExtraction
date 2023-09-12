24e3a8131e974975c38e08b96afc50b6ff56a270
Tagir Melnikov <Melnikov@gmail.ru>
2023-09-12 23:01:03 +0700

    добавил передачу файла из вью в контроллер

---
+++ WordExtraction/Controllers/ExtractController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WordExtraction.Controllers;
+
+public class ExtractController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> UploadFile(IFormFile file)
+    {
+        using (var fileStream = file.OpenReadStream())
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                await fileStream.CopyToAsync(memoryStream);
+            }
+        }
+        
+        return Ok("Файл успешно обработан.");
+    }
+}

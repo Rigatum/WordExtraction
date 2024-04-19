4c571ef22df23073cdb66dd50123ae47677b88ac
Rigatum <tagirs1000@gmail.com>
2024-04-19 21:33:14 +0700

    refactoring

--- WordExtraction/Services/TranslateService.cs
+++ WordExtraction/Services/TranslateService.cs
@@ -10,22 +10,27 @@ namespace WordExtraction.Services
         {
             _httpClientFactory = httpClientFactory;
         }
-
+         
         public async Task<string> TranslateAsync(IEnumerable<string> words, string sourceLanguage, string targetLanguage)
         {
-            var httpClient = _httpClientFactory.CreateClient();
+            var httpClient = CreateHttpClient(_httpClientFactory);
 
             var solutionPath = Directory.GetParent(Environment.CurrentDirectory)?.FullName;
+
             string apiKey = GetApiKey(solutionPath).Result;
 
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"Api-Key {apiKey}");
+            AddHeadersForAuthorization(httpClient, "Authorization", $"Api-Key {apiKey}");
 
             var json = new StringContent(TransformWordsToJson(sourceLanguage, targetLanguage, words).Result);
 
             return await httpClient.PostAsync(new Uri("https://translate.api.cloud.yandex.net/translate/v2/translate"), json).Result.Content.ReadAsStringAsync();
         }
 
+        public HttpClient CreateHttpClient(IHttpClientFactory httpClientFactory) => httpClientFactory.CreateClient();
+
         public async Task<string> GetApiKey(string path) =>  await File.ReadAllTextAsync($"{path}/test.env");
+
+        public void AddHeadersForAuthorization(HttpClient httpClient, string name, string value) => httpClient.DefaultRequestHeaders.Add(name, value);
 
         public async Task<string> TransformWordsToJson(string sourceLanguage, string targetLanguage, IEnumerable<string> words)
         {

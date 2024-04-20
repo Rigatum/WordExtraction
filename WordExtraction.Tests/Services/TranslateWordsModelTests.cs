6f8c8a07fb6d0ff8cf4e99019a431f1ecb42e170
Rigatum <tagirs1000@gmail.com>
2024-04-20 18:34:10 +0700

    add test for ctor

---
+++ WordExtraction.Tests/Services/TranslateWordsModelTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using WordExtraction.Models;
+
+namespace WordExtraction.Tests.Services;
+
+public class TranslateWordsModelTests
+{
+    [Theory]
+    [InlineData("de", "ru", new[] {"car", "mother"})]
+    [InlineData("ru", "de", new[] {""})]
+    [InlineData("de", "en", new[] {"324"})]
+    [InlineData("de", "en", new[] {"polizei", "pferd"})]
+    public void TranslateWordsModelConstructor(string sourceLanguage, string targetLanguage, IEnumerable<string> words)
+    {
+        var translateWordsModel = new TranslateWordsModel(sourceLanguage, targetLanguage, words);
+
+        translateWordsModel.SourceLanguage.Should().BeEquivalentTo(sourceLanguage);
+        translateWordsModel.TargetLanguage.Should().BeEquivalentTo(targetLanguage);
+        translateWordsModel.Words.Should().HaveSameCount(words);
+        translateWordsModel.Words.Should().BeEquivalentTo(words);
+    }
+}

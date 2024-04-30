30fece9ed3ab5da0e6ff9a04afdd2bbc8e9e6ba6
Rigatum <tagirs1000@gmail.com>
2024-05-01 01:33:46 +0700

    add test

---
+++ WordExtraction.Tests/Models/ErrorViewModelTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using WordExtraction.Models;
+
+namespace WordExtraction.Tests.Models;
+
+public class ErrorViewModelTests
+{
+    [Theory]
+    [InlineData("sd")]
+    [InlineData("2341223")]
+    [InlineData("$#%*#$is")]
+    [InlineData(" ")]
+    public void Properties_WithNotNullArguments_Succeeds(string? requestId)
+    {
+        var errorViewModel = Setup();
+
+        errorViewModel.RequestId = requestId;
+
+        using (new AssertionScope())
+        {
+            errorViewModel.RequestId.Should().Be(requestId);
+            errorViewModel.ShowRequestId.Should().BeTrue();
+        }
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void Properties_WithNullArguments_Succeeds(string? requestId)
+    {
+        var errorViewModel = Setup();
+
+        errorViewModel.RequestId = requestId;
+
+        using (new AssertionScope())
+        {
+            errorViewModel.RequestId.Should().Be(requestId);
+            errorViewModel.ShowRequestId.Should().BeFalse();
+        }
+    }
+
+    private ErrorViewModel Setup() => new();
+}

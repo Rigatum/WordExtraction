21a424e300638b067bfeff3f9782648138f523ca
Rigatum <tagirs1000@gmail.com>
2024-05-01 14:32:24 +0700

    add tests

---
+++ WordExtraction.Tests/ServiceTests/ReadStrategy/MicrosoftWordReadTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using WordExtraction.Services.ReadStrategy;
+
+namespace WordExtraction.Tests.ServiceTests.ReadStrategy;
+
+public class MicrosoftWordReadTests
+{
+    [Fact]
+    public void ReadAsync_NotImplemented_ShouldThrowNotImplementedException()
+    {
+        var microsoftWordRead = new MicrosoftWordRead();
+        var formFile = SetupMock();
+
+        Action act = () => microsoftWordRead.ReadAsync(formFile);
+
+        act.Should().Throw<NotImplementedException>();
+    }
+
+    private IFormFile SetupMock() => new Mock<IFormFile>(MockBehavior.Strict).Object;
+}
---
+++ WordExtraction.Tests/ServiceTests/ReadStrategy/PdfReadTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using WordExtraction.Services.ReadStrategy;
+
+namespace WordExtraction.Tests.ServiceTests.ReadStrategy;
+
+public class PdfReadTests
+{
+    [Fact]
+    public void ReadAsync_ValidPdfFile_Succeeds()
+    {
+        var (formFile, pdfRead) = SetupMock();
+
+        StringBuilder text = pdfRead.ReadAsync(formFile).Result;
+
+        text.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ReadAsync_WhenNullFormFile_NullReferenceException()
+    {
+        var (_, pdfRead) = SetupMock();
+
+        Func<Task> func = () => pdfRead.ReadAsync(null!);
+
+        func.Should().ThrowAsync<NullReferenceException>();
+    }
+
+    private (IFormFile, PdfRead) SetupMock()
+    {
+        var byteArray = Properties.Resources.Pride_and_Prejudice_by_Jane_Austen_6;
+
+        var stream = new MemoryStream(byteArray);
+
+        var file = new FormFile(stream, 0, stream.Length, null!, "PdfTest");
+
+        return (file, new PdfRead());
+    }
+}

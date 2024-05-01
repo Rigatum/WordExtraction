b18331e461ba37b505ae1415caac7ad5e55aef79
Rigatum <tagirs1000@gmail.com>
2024-05-01 18:15:57 +0700

    add test

---
+++ WordExtraction.Tests/ServiceTests/FileProcessServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using WordExtraction.Services.FileProcessService;
+using WordExtraction.Services.ReadStrategy;
+
+namespace WordExtraction.Tests.ServiceTests;
+
+public class FileProcessServiceTests
+{
+    [Fact]
+    public void SetTypeRead_SetValueToProperty()
+    {
+        var translateServiceMock = new Mock<IFileProcessService>();
+
+        var fileProcessService = SetupMockForSetTypeRead(translateServiceMock);
+
+        fileProcessService.SetTypeRead(new PdfRead());
+
+        translateServiceMock.VerifyAll();
+    }
+
+    private IFileProcessService SetupMockForSetTypeRead(Mock<IFileProcessService> fileProcessServiceMock)
+    {
+        fileProcessServiceMock
+            .Setup(f => f.SetTypeRead(It.IsAny<ITypeRead>()))
+            .Verifiable(Times.Once);
+
+        return fileProcessServiceMock.Object;
+    }
+}

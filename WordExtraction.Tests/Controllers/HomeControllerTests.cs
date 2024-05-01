5ae8434d510f5db3f6b87a3df0575a8b6bfd6e27
Rigatum <tagirs1000@gmail.com>
2024-05-01 19:02:42 +0700

    add test

---
+++ WordExtraction.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using WordExtraction.Controllers;
+
+namespace WordExtraction.Tests.Controllers;
+
+public class HomeControllerTests
+{
+    [Fact]
+    public void Index_ActionExecutes_ReturnsViewForIndex()
+    {
+        var logger = new Mock<ILogger<HomeController>>(MockBehavior.Strict).Object;
+        var result = new HomeController(logger).Index();
+
+        result.Should().BeOfType<ViewResult>();
+    }
+
+    [Fact]
+    public void Privacy_ActionExecutes_ReturnsViewForPrivacy()
+    {
+        var logger = new Mock<ILogger<HomeController>>(MockBehavior.Strict).Object;
+        var result = new HomeController(logger).Privacy();
+
+        result.Should().BeOfType<ViewResult>();
+    }
+}
